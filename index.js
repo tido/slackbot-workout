e0b40f4eededad0aa6c5e7fc9eb564c4906c67e6
Clarkie <andrew.t.clarke@gmail.com>
2017-07-21 10:13:43 +0100

    feat: only ping online members during office hours

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { shuffle, slice, filter, flow, get, map, join, sample, random } = require('lodash/fp');
+const { shuffle, slice, filter, flow, get, map, join, sample, random, zipWith, set } = require('lodash/fp');
 const axios = require('axios');
 const co = require('co');
+const moment = require('moment');
 
 const config = require('./config');
-const { callouts, exercises } = config;
+const { callouts, exercises, officeHours } = config;
 
 const sendMessage = message => axios.post(config.webhook, {
   channel: config.channel,
@@ -13,10 +14,10 @@ const sendMessage = message => axios.post(config.webhook, {
 });
 
 const channelInfoUrl = `https://slack.com/api/channels.info?token=${config.token}&channel=${config.channelId}`;
+const userStatusUrl = userId => `https://slack.com/api/users.getPresence?token=${config.token}&user=${userId}`;
 
 const wait = minutes => done => setTimeout(done, minutes * 60 * 1000);
 const pickParticipants = flow(
-  get('channel.members'),
   shuffle,
   slice(0, callouts.numPeople),
   map(id => `<@${id}>`),
@@ -31,9 +32,25 @@ pickExercise = flow(
 co(function* () {
   yield sendMessage('ok, are you ready to pump?!');
 
-  while(true) {
+  while (true) {
     const { data: channelInfo } = yield axios.get(channelInfoUrl);
-    const participants = pickParticipants(channelInfo);
+    console.log(channelInfo);
+    const { members } = channelInfo.channel;
+    const memberStatusRequests = flow(
+      map(userStatusUrl),
+      map(url => axios.get(url).then(get('data')))
+    )(members);
+    const memberStatuses = yield memberStatusRequests;
+    const onlineMembers = flow(
+      zipWith(set('id'), members),
+      filter({ presence: 'active' }),
+      map('id')
+    )(memberStatuses)
+
+    console.log(onlineMembers);
+
+    const participants = pickParticipants(onlineMembers);
+    console.log('picked participants', participants);
 
     const {
       minReps,
@@ -41,10 +58,22 @@ co(function* () {
       name,
       units,
     } = pickExercise(exercises);
+    console.log('picked exercise', name);
 
-    yield sendMessage(`${participants} - ${random(minReps, maxReps)} ${units} ${name} NOW PLEASE!`);
+    const message = `${participants} - ${random(minReps, maxReps)} ${units} ${name} NOW PLEASE!`;
+    yield sendMessage(message);
+    console.log('sent message', message);
 
-    yield wait(random(callouts.minutesBetween.min, callouts.minutesBetween.max));
+    const minutesToWait = random(callouts.minutesBetween.min, callouts.minutesBetween.max);
+    const nextRunTime = moment().add(minutesToWait, 'minutes');
+
+    if (nextRunTime.hours() > officeHours.end) {
+      const outOfHoursWaitTime = (officeHours.begin + 24 - officeHours.end) * 60
+      console.log(`out of hours so waiting for ${outOfHoursWaitTime} minutes`);
+      yield wait(outOfHoursWaitTime);
+    }
+    console.log(`next excersie in ${minutesToWait} minutes`);
+    yield wait(minutesToWait);
   }
-
-});
+})
+.catch(console.error);
